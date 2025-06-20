6a3cf08eefb5409c765f4031335280880353b379
Julian Compton <julian.c.compton@gmail.com>
2025-06-19 23:47:36 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,121 @@
+#include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "liblvgl/lvgl.h"
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER); // sets up controller
+pros::MotorGroup rightDrive({1, -2, 3}, pros::MotorGearset::blue); // creates the right drivetrain motor group with forwards ports 1 & 3 and backwards port 2
+pros::MotorGroup leftDrive({4, -5, 6}, pros::MotorGearset::blue); // creates the left drivetrain motor group with forwards ports 4 & 6 and backwards port 5
+
+// drivetrain settingsMore actions
+lemlib::Drivetrain drivetrain(&leftDrive, // left motor group
+							  &rightDrive, // right motor group
+							  13.5, // 13.5 inch track width (NEEDS TO BE MEASURED)
+							  lemlib::Omniwheel::NEW_325, // using new 3.25" omnis
+							  480, // drivetrain rpm is 480 rpm
+							  2 // horizontal drift is 2 (for now)
+);
+
+
+// odometry settings
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null (temporary)
+  							nullptr, // vertical tracking wheel 2, set to null
+  							nullptr, // horizontal tracking wheel 1, set to null (temporary hopefully)
+  							nullptr, // horizontal tracking wheel 2, set to null
+  							nullptr // inertial sensor, set to null (temporary)
+);
+
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+						lateral_controller, // lateral PID settings
+						angular_controller, // angular PID settings
+						sensors // odometry sensors
+);
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	while (true) {
+		int tankLeft = controller.get_analog(ANALOG_LEFT_Y); // the left analog stick controls the left side of the drive train
+        int tankRight = controller.get_analog(ANALOG_RIGHT_Y); // the right analog stick controls the left side of the drive train        
+        chassis.tank(tankLeft, tankRight); // creates a tank drive scheme
+		pros::delay(25); // wait 25ms to save resources
+	}
+}
